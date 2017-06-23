e4c9cedb12853e82c04ace06f4906c99ec3f4e75
王云捷 <wangyunjie@hotmail.com>
2017-06-23 16:11:36 +0800

    添加HasFeatureReturnsTrueForSupportedFeature测试

---
+++ TagKit.Markup.Document.Tests/TagImplementationTests/HasFeatureTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TagKit.Markup.Tests.TagImplementationTests
+{
+    public class HasFeatureTests
+    {
+        // Feature supported by the TagImplementation
+        private const string SupportedFeature = "XML";
+        [Fact]
+        public static void HasFeatureReturnsTrueForSupportedFeature()
+        {
+            var target = new TagImplementation();
+            // verify that it's also case-insensitive
+            Assert.True(target.HasFeature(SupportedFeature.ToUpper(), null));
+            Assert.True(target.HasFeature(SupportedFeature.ToLower(), null));
+        }
+
+    }
+}
