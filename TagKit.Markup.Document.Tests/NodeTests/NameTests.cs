188ad927ffb0cb339ffbeebb23c39c2a51883eb7
王云捷 <wangyunjie@hotmail.com>
2017-06-23 16:32:52 +0800

    添加XmlDeclaration测试

---
+++ TagKit.Markup.Document.Tests/NodeTests/NameTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace TagKit.Markup.Tests.NodeTests
+{
+    public class NameTests
+    {
+        [Fact]
+        public static void XmlDeclaration()
+        {
+            var xmlDocument = new Document();
+            xmlDocument.LoadXml("<?xml version=\"1.0\" standalone=\"no\"?><root />");
+
+            Assert.Equal("xml", xmlDocument.FirstChild.Name);
+        }
+    }
+}
