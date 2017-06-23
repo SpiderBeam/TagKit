922b7ef347b3d3005daa99d6aa38ee4277f83aa6
王云捷 <wangyunjie@hotmail.com>
2017-06-23 15:47:42 +0800

    添加CreateDocument文档测试

---
+++ TagKit.Markup.Document.Tests/TagImplementationTests/CreateDocumentTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TagKit.Markup.Tests 
+{
+    public class CreateDocumentTests
+    {
+        [Fact]
+        public static void CreateDocument()
+        {
+            var imp = new TagImplementation();
+
+            var doc1 = imp.CreateDocument();
+            var doc2 = imp.CreateDocument();
+
+            Assert.NotNull(doc1);
+            Assert.NotNull(doc2);
+            Assert.NotSame(doc1, doc2);
+        }
+    }
+}
